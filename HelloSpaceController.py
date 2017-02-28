980c9e7f93c708d3b06556f0b9df9d8c8eb62eb9
izumida <izumida@birdman.ne.jp>
2017-02-28 16:00:57 +0900

    CodeGenerator ひな形作成

--- HelloSpaceController.py
+++ HelloSpaceController.py
@@ -3,6 +3,41 @@ from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.action_chains import ActionChains
 import time
 
+class CodeGenerator:
+	@classmethod
+	def generate(cls, paramList):
+		code = []
+		code.append('var i = 0;')
+		code.append('return function GoToMoon(state) {')
+		code.append('var rocket = state.rocket')
+		code.append('var moon = state.planetStates[1]')
+		code.append(cls.generateLogCode())
+		code.append('var controls = new Controls({ thrust: 1, rcs: { pitch: 0.01, yaw: 0.01 }});')
+		code.append('i++;')
+		code.append('return controls;')
+		return code
+
+	@classmethod
+	def generateLogCode(cls):
+		str = ''
+		str += 'console.log("state", '
+		str += 'i, '
+		str += 'rocket.mass, '
+		str += 'rocket.position.x, rocket.position.y, rocket.position.z, '
+		str += 'rocket.rotation.x, rocket.rotation.y, rocket.rotation.z, rocket.rotation.w, '
+		str += 'rocket.velocity.x, rocket.velocity.y, rocket.velocity.z, '
+		str += 'rocket.angularVelocity.x, rocket.angularVelocity.y, rocket.angularVelocity.z, '
+		str += 'rocket.exploded, '
+		str += 'rocket.fuel.mass, rocket.fuel.volume, '
+		str += 'moon.mass, '
+		str += 'moon.radius, '
+		str += 'moon.position.x, moon.position.y, moon.position.z, '
+		str += 'moon.rotation.x, moon.rotation.y, moon.rotation.z, '
+		str += 'moon.velocity.x, moon.velocity.y, moon.velocity.z, '
+		str += 'moon.angularVelocity.x, moon.angularVelocity.y, moon.angularVelocity.z'
+		str += ');'
+		return str
+
 class UIController:
 	def __init__(self, driver):
 		self.driver = driver
@@ -56,9 +91,11 @@ class HelloSpaceController:
 		self.driver.close()
 
 	def setCode(self, code):
+		genCode = CodeGenerator.generate(code)
+	
 		self.editor.activate()
 		self.editor.deleteAll()
-		for str in code:
+		for str in genCode:
 			self.editor.typeLine(str)
 
 	def reset(self):
@@ -73,10 +110,7 @@ if __name__ == '__main__':
 	controller = HelloSpaceController()
 	controller.connect()
 
-	code = [
-		"return function GoToMoon(state) {",
-		"return new Controls({ thrust: 1, rcs: { pitch: 0.01, yaw: 0.01 }})"
-	]
+	code = []
 	controller.setCode(code)
 
 	controller.launch()
